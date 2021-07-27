96c144a8945e81d113bf5c760e82ef2dc06c82bb
Mike Zorin <mike.zorin@robertsoncollege.ca>
2021-07-27 13:41:41 -0500

    Add project files.

---
+++ WebAPIApplication2/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIApplication2.Models;
+
+namespace WebAPIApplication2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt){}
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProducts> OrderProducts { get; set; }
+    }
+}
---
+++ WebAPIApplication2/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIApplication2.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public DateTime Date { get; set; }
+
+        [NotMapped]
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
